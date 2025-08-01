49fb987033d226d183a73ba97df7156fe8a789d0
junhyunging <166470490+junhyunging@users.noreply.github.com>
2025-08-01 12:44:12 +0900

    08-01 수업내용

---
+++ 6.Flask/9.login/bcrypt_example.py
@@ -0,0 +1,22 @@
+#pip install bcrypt
+import bcrypt
+
+# bcrypt 내부 함수..
+
+def generate_hash(password):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode(),salt)
+    return hashed
+
+def verify_password(password,hash_password):
+    return bcrypt.checkpw(password.encode(),hash_password)
+
+hashed1 = generate_hash('hello123')
+hashed2 = generate_hash('hello123')
+
+print('Hash1' , hashed1)
+print('Hash2' , hashed2)
+
+print("검증",verify_password("hello222",hashed1))
+print("검증",verify_password("hello123",hashed1))
+
---
+++ 6.Flask/9.login/init_database2_bcrypt.py
@@ -0,0 +1,28 @@
+import sqlite3
+import bcrypt
+
+DB_FILENAME = 'users.db'
+
+conn = sqlite3.connect(DB_FILENAME)
+cur = conn.cursor()
+
+
+#테이블 생성
+cur.execute('''
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    name TEXT NOT NULL
+)
+            ''')
+
+def create_user(username,password,name):
+    hashed_pw = bcrypt.hashpw(password.encode(),bcrypt.gensalt())
+    conn = sqlite3.connect(DB_FILENAME)
+    cur = conn.cursor()
+    cur.execute("INSERT INTO users (username,password,name) VALUES(?,?,?)",(username,hashed_pw,name))
+    conn.commit()
+    conn.close()
+
+    
